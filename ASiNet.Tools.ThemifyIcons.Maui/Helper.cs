d02d9965f6fa2937cad19adf23cba2b397b05121
Александр Лычкин <Alexander-Si.World@yandex.ru>
2022-12-01 17:10:27 +0300

    ThemifyIcons from MAUI!

---
+++ ASiNet.Tools.ThemifyIcons.Maui/Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASiNet.Tools.ThemifyIcons.Maui;
+public static class Helper
+{
+    /// <summary>
+    /// String: "themify.ttf"
+    /// </summary>
+    public const string FontConnectedString = "themify.ttf";
+
+}
